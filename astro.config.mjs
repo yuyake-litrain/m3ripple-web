62ef97c530952cbafde80c3c88682cd9397de640
yuyake-litrain <85449119+yuyake-litrain@users.noreply.github.com>
2025-06-22 06:57:30 +0900

    Fix: set appropliate root path to the js.org (info: m-h-san is me)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://m-h-san.github.io',
-  base: '/m3ripple-web',
+  site: 'https://m3ripple.js.org',
+  base: '/',
   trailingSlash: 'always',
   integrations: [react(), mdx(), swup({ theme: false, globalInstance: true })],
 });
