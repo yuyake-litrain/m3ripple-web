4b70474ed100e348e2197444ce522ec29d5d24ad
yuyake-litrain <85449119+yuyake-litrain@users.noreply.github.com>
2025-06-22 21:45:27 +0900

    Add: Home Page

---
+++ src/components/IconLink.tsx
@@ -0,0 +1,37 @@
+import { Icon } from "@iconify/react";
+import { RippleContainer } from "@m_three_ui/m3ripple";
+import clsx from "clsx";
+import styles from "./IconLink.module.css";
+
+const SidebarSelectButton = ({
+  iconifyIcon,
+  className,
+  href,
+}: {
+  iconifyIcon: string;
+  className?: string;
+  href: string;
+}) => {
+  return (
+    <a
+      className={clsx(className, "SidebarSelectButton", styles.link)}
+      href={href}
+    >
+      <RippleContainer
+        isMaterial3={true}
+        className={clsx(styles.rippleContainer, className)}
+        rippleColor="hsla(29,97%,75%,0.15)"
+        sparklesColorRGB="255 255 255"
+        opacity_level1="0.4"
+        opacity_level2="0.1"
+      >
+        <Icon
+          className={styles.icon}
+          icon={iconifyIcon}
+        />
+      </RippleContainer>
+    </a>
+  );
+};
+
+export default SidebarSelectButton;
---
+++ src/components/rippleTester.tsx
@@ -0,0 +1,44 @@
+import { RippleContainer } from "@m_three_ui/m3ripple";
+import styles from "./rippleTester.module.css";
+import { useState } from "react";
+import { useReward } from "react-rewards";
+
+const rippleTester = () => {
+  const { reward } = useReward("rewardId", "confetti", {
+    colors: ["#ffae00", "#ac7600", "#ac4a00", "#ffe600", "#a47100"],
+        spread: 120,
+        lifetime: 60,
+    decay: 0.92
+  });
+  const [isConfettiEnabled, setIsConfettiEnabled] = useState<boolean>(true);
+
+  return (
+    <div className={styles.container}>
+      <RippleContainer
+        className={styles.ripple_container}
+        sparklesColorRGB="255 255 255"
+        opacity_level1="0.4"
+        opacity_level2="0.1"
+        divProps={{
+          onClick: () => {
+            if (isConfettiEnabled) reward();
+          },
+        }}
+      >
+        <span id="rewardId" className={styles.confettiOrigin}></span>
+        RippleContainer component
+      </RippleContainer>
+
+      <label>
+        <input
+          type="checkbox"
+          checked={isConfettiEnabled}
+          onChange={(e) => setIsConfettiEnabled(e.target.checked)}
+        />
+        Enable Confetti
+      </label>
+    </div>
+  );
+};
+
+export default rippleTester;
